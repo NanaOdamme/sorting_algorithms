b025838deafdef89167a708a56af527e43c29712
NanaOdamme <eronastyles@gmail.com>
2023-11-20 02:05:37 -0800

    code update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -51,4 +51,35 @@ void bubble_sort(int *array, size_t size)
 		if (swapped == 0)
 			break;
 	}
-}
+void bubble_sort(int *array, size_t size)
+{
+        int swapped;
+        size_t i, j, k;
+
+        if (array == NULL || size == 0)
+                return;
+
+        for (i = 0; i < size - 1; i++)
+        {
+                swapped = 0;
+                for (j = 0; j < size - i - 1; j++)
+                {
+                        if (array[j] > array[j + 1])
+                        {
+                                swap(&array[j], &array[j + 1]);
+                                swapped = 1;
+                                /* Print the array after each swap */
+                                for (k = 0; k < size; k++)
+                                {
+                                        printf("%d", array[k]);
+                                        if (k < size - 1)
+                                                printf(", ");
+                                }
+                                printf("\n");
+                        }
+                }
+                /* If no two elements were swapped, the array is already sorted */
+                if (swapped == 0)
+                        break;
+        }
+}}
