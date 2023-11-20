8eca860bca035b8966e517a6dba69a5013949e92
NanaOdamme <eronastyles@gmail.com>
2023-11-20 01:32:17 -0800

    107 code update

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -1,1 +1,91 @@
+#include <stdio.h>
+#include "sort.h"
 
+/**
+ * swap - Swaps two integers in an array.
+ * @a: Pointer to the first integer.
+ * @b: Pointer to the second integer.
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * hoare_partition - Implements the Hoare partition scheme.
+ * @array: The array to be partitioned.
+ * @low: The low index of the partition.
+ * @high: The high index of the partition.
+ * @size: The size of the array.
+ *
+ * Return: The index of the pivot after partitioning.
+ */
+int hoare_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1;
+	int j = high + 1;
+
+	while (1)
+	{
+		do
+		{
+			i++;
+		} while (array[i] < pivot);
+
+		do
+		{
+			j--;
+		} while (array[j] > pivot);
+
+		if (i < j)
+		{
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+		else
+		{
+			return (j);
+		}
+
+		if (i == j)
+		{
+			return (j);
+		}
+	}
+}
+
+/**
+ * quick_sort_recursive - Recursively sorts an array using the Hoare scheme.
+ * @array: The array to be sorted.
+ * @low: The low index of the partition.
+ * @high: The high index of the partition.
+ * @size: The size of the array.
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int pivot_index;
+
+	if (low < high)
+	{
+		pivot_index = hoare_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot_index, size);
+		quick_sort_recursive(array, pivot_index + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort_hoare - Initiates the Quick Sort using the Hoare scheme.
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
